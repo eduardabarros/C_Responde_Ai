bcd254e25d07b426e7f68ab802f635a1cd450ecf
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-05-26 09:18:00 -0300

    Adicionando arquivos via upload
    Programas criados no terminal do Ubuntu

---
+++ exemplos_e_exercicios/1.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream> // iostream: é um header que contém a função de escrita. Dentro do iostream temos várias funções para entrada e saída de dados
+
+using namespace std; // using namespace:  avisa ao compilador sobre a origem das funções do header. std: porque estamos usando apenas a biblioteca padrão. É uma sigla para 'standard'.
+
+int main() // a função main: função principal do programa e ela diz onde o seu código começa e termina.
+
+     {
+     
+     cout <<"Olá, Mundo!" << endl;
+     
+     // count: função que escreve textos na tela. Se pronuncia "cê out" e significa "console output".
+     
+    // o símbolo << é o operador de inserção. 
+    
+    // endl: "end line" que significa fim da linha (pula uma linha entre o texto e o fim do programa).
+     
+     return 0; // informa o final do nosso programa
+     
+     }
+     
+     // O C++ padrão não aceita acentos. Mas esse aceitou e imprimiu direitinho aqui no meu pc.
+     
+     // Fonte: Responde Ai - Exemplo
