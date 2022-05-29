b699166858754eac0ef5782cfaad174bed70a6bf
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-05-29 10:59:33 -0300

    Adicionando arquivos via upload

---
+++ exemplos_e_exercicios/20.cpp
@@ -0,0 +1,23 @@
+
+	#include<iostream>
+	using namespace std;
+	int main ()
+	
+	{
+	
+	char caracter;
+	short int num2;
+	float num3;
+	double num1;
+	
+	cout << "Um char ocupa " << sizeof (caracter) << " byte(s)" << endl;
+	cout << "Um short int ocupa " << sizeof (num2) << " byte(s)" << endl;
+	cout << "Um float ocupa " << sizeof (num3) << " byte(s)" << endl;
+	cout << "Um double ocupa " << sizeof (num1) << " byte(s)" << endl;
+	
+	return 0;
+	
+	}
+	
+	// Fonte: Responde Aí - Exercícios
+	// As variáveis estão em ordem crescente de memória ocupada.
