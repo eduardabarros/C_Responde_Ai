4cb8599a8214ded8502df0757c83aa152a6d3df2
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-05-29 10:53:27 -0300

    Adicionando programas via upload

---
+++ exemplos_e_exercicios/18.cpp
@@ -0,0 +1,16 @@
+
+	#include<iostream>
+	using namespace std;
+	int main()
+	
+	{
+	
+	unsigned int Numero;
+	
+	return 0;
+	
+	}
+	
+	// Exemplo: uma distância muito grande é um valor positivo mas que ultrapassa a capacidade do int. Usamos o modificador de variável 'usingned' assim estamos indicando para o programa que a variável é sempre positiva, com isso, ele "move" o intervalo de valores que ela pode assumir para cobrir apenas valores positivos. Então o limite máximo é aumentado.
+	
+	// Fonte: Responde Aí - Exemplo
