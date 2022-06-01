3faca46d5ad11c33258c8c6434ff96d8bbdcc91a
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-06-01 13:41:57 -0300

    Adicionando arquivos via upload

---
+++ exemplos_e_exercicios/26.cpp
@@ -0,0 +1,19 @@
+
+	#include<iostream>
+	using namespace std;
+	int main()
+
+	{
+	
+	cout << "Lista de compras: \n"
+	     << "Leite \t 2L \n"
+	     << "Carne \t 500 g \n"
+	     << "Ovos \t uma duzia"
+	     << endl;
+	     
+	return 0;
+
+	}
+	
+	// Fonte: Responde Aí - Exemplos
+	// Mostra a utilizada do \n e \t
