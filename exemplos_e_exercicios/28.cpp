b225589327db05e50ce9298ed5c5ec26eb533ab0
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-06-09 16:50:01 -0300

    Adicionando arquivos via upload

---
+++ exemplos_e_exercicios/28.cpp
@@ -0,0 +1,23 @@
+
+	#include<iostream>
+	using namespace std;
+	int main ()
+	
+	{
+	
+	char a;
+	
+	cout << endl << "Digite um caracter e aperte ENTER: ";
+	
+	cin >> a;
+	
+	cout << "O caracter digitado foi: ";
+	
+	cout << a << endl << endl;
+	
+	return 0;
+	
+	}
+	
+	// Fonte: Responde Aí - Exercício
+	// Utilização da função chair
