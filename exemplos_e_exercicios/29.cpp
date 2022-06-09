e8e1964c091c104d83c1f399054f5499964ae843
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-06-09 17:03:22 -0300

    Adicionando arquivos via upload

---
+++ exemplos_e_exercicios/29.cpp
@@ -0,0 +1,29 @@
+
+	#include<iostream>
+	using namespace std;
+	int main ()
+	
+	{
+	
+	int a, b, soma;
+	
+	cout << endl << "Digite um numero inteiro e aperte ENTER: ";
+	
+	cin >> a;
+	
+	cout << "Digite novamente um numero inteiro e aperte ENTER: ";
+	
+	cin >> b;
+	
+	soma = a + b;
+	
+	cout << "O resultado apresentado é a soma dos numeros digitados anteriormente: ";
+	
+	cout << soma << endl << endl;
+	
+	return 0;
+	
+	}
+	
+	// Fonte: Responde Aí - Exercícios
+	// Soma de dois números inteiros
