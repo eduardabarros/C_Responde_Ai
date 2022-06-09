b89200646c1347113a70cad35c356afef6d7705f
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-06-09 16:41:01 -0300

    Adicionando arquivos via upload

---
+++ exemplos_e_exercicios/27.cpp
@@ -0,0 +1,30 @@
+
+	#include<iostream>
+	using namespace std;
+	int main ()
+	
+	{
+	
+	int a;
+	int b;
+	int sub;
+	
+	cout << endl << "Digite um numero inteiro e aperte ENTER: ";
+	
+	cin >> a;
+	
+	cout << "Digite novamente um numero inteiro e aperte ENTER: ";
+	
+	cin >> b;
+	
+	cout << "O valor da subtracao entre o segundo numero digitado e o primeiro é: ";
+	
+	sub = b - a;
+	
+	cout << sub << endl << endl;
+	
+	return 0;
+	
+	}
+	
+	// Fonte: Responde Aí - Exercícios
