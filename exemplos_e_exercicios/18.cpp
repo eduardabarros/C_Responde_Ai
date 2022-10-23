aec7d9a5ee7f2dd6077b2c373e73b577039d1a47
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-10-23 09:45:04 -0300

    Adicionando arquivos via upload

--- exemplos_e_exercicios/18.cpp
+++ exemplos_e_exercicios/18.cpp
@@ -5,7 +5,7 @@
 	
 	{
 	
-	unsigned int Numero;
+	unsigned int Numero;   // Indicamos para o programa que a variável em questão é sempre positiva, com isso ele 'move' o intervalo de valores que ela pode assumir
 	
 	return 0;
 	
@@ -13,4 +13,4 @@
 	
 	// Exemplo: uma distância muito grande é um valor positivo mas que ultrapassa a capacidade do int. Usamos o modificador de variável 'usingned' assim estamos indicando para o programa que a variável é sempre positiva, com isso, ele "move" o intervalo de valores que ela pode assumir para cobrir apenas valores positivos. Então o limite máximo é aumentado.
 	
-	// Fonte: Responde Aí - Exemplo
+	// Fonte: Responde Aí - Memória e Modificadores
