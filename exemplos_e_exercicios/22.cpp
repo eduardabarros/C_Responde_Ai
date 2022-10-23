f7b5f36e3db6ae6e58e6bf93924fbb90c3c443cc
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-10-23 09:49:56 -0300

    Adicionando arquivos via upload

--- exemplos_e_exercicios/22.cpp
+++ exemplos_e_exercicios/22.cpp
@@ -18,4 +18,4 @@
 	
 	// O erro do programa é que ao invés de usar o float tem que usar o double. Mas quando pedi para printar na tela só apareceu até o 9 (5 casas decimais). Tentei usar o unsigned mas não adiantou neste caso.
 	
-	// Fonte: Responde Aí - Exercício
+	// Fonte: Responde Aí - Memória e Modificadores
