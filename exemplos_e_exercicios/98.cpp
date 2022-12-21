b0cc60e962efc6d9d46b1d8b59857174e08e825e
Eduarda de Barros <98418040+eduardabarros@users.noreply.github.com>
2022-12-21 18:43:34 -0300

    Adicionando arquivos via upload

---
+++ exemplos_e_exercicios/98.cpp
@@ -0,0 +1,28 @@
+
+	#include <iostream>
+	
+	using namespace std;
+	
+	int main ()
+	
+	{
+	
+	int max;
+	int i = 0;	// Variável contadora
+	
+	cout << endl << "\tQual o numero? ";
+	cin >> max;
+	
+	while ( i <= max )
+	
+		{ cout << "\t" << i << endl;  // Código que queremos repetir
+		
+		  i++;  // Atualizando a expressão do laço
+		  
+		 }
+		
+	return 0;
+	
+	// Fonte: Responde Aí - Estruturas de repetição: while e do-while	
+	
+	}
---
+++ exemplos_e_exercicios/99.cpp
@@ -0,0 +1,27 @@
+
+	#include <iostream>
+	#include <math.h>
+	
+	using namespace std;
+	
+	int main ()
+	
+	{
+	
+	int num;
+	int i = 1;
+	
+	cout << endl << "\tDigite um número: ";
+	cin >> num;
+	
+	while ( i <= 10 )
+	
+		{ cout << "\t" << i << " x " << num << " = " << num*i << endl;
+		
+		  i++; }
+		
+	return 0;
+	
+	// Fonte: Responde Aí - Estruturas de repetição: while e do-while
+	
+	}
